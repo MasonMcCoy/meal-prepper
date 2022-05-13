5232eb7433afccdbcee1fac246b2e3e0853a02e3
Mason McCoy <mmasonmccoy@gmail.com>
2022-05-13 18:55:19 -0500

    Recipe information from the API request now renders into a grid on the page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 // API Credentials
-var appID = "4238a1ff";
-var appKey = "c7d57c2318b4a47dee8151c302c2a3cb";
+var appID = config.app_id;
+var appKey = config.app_key;
 
 var searchBttn = $("#search-button");
+var contentContainer = $("#content");
 
 // Captures user input as query term for API call
-function getResponse(event) {
+function getRecipes(event) {
     event.preventDefault();
 
     var searchTerm = $("#search-term").val();
@@ -13,75 +14,117 @@ function getResponse(event) {
     var baseURL = "https://api.edamam.com/api/recipes/v2?type=public&q=";
     var call = baseURL + searchTerm + "&app_id=" + appID + "&app_key=" + appKey;
 
+    console.log(call); 
+
     $.ajax({url: call, success: function(response) {
         console.log(response);
 
-        for (var i = 0; i < response.hits.length; i++) {
-            // Lists names for 20 Recipes (more if we want to paginate)
-            console.log(response.hits[i].recipe.label);
-
-            for (var j = 0; j < response.hits[i].recipe.ingredientLines.length; j++) {
-                // Lists ingredients (there is a more granular way to do this, should we find it necessary)
-                console.log(response.hits[i].recipe.ingredientLines[j]);
-            }
-
-            // REMOVE THIS WHEN WE AREN'T LOGGING TO CONSOLE
-            console.log("-----");
-        }
+        var recipes = response.hits;
+        renderRecipes(recipes);
     }})
 }
 
+function renderRecipes(recipesResponse) {
+    for (var i = 0; i < recipesResponse.length; i++) {
+        var recipeCard = $("<div>").attr("class", "card recipe-card");
+        
+        // CARD TITLE
+        var recipeName = $("<div>").attr("class", "card-hearder");
+        
+        // Recipe Name
+        recipeName.append($("<div>").attr("class", "card-hearder-title").text(recipesResponse[i].recipe.label));
+        console.log(recipesResponse[i].recipe.label);
 
-searchBttn.on("click", getResponse);
+        // CARD IMAGE
+        var recipeImg = $("<div>").attr("class", "card-image");
+        
+        // Thumbnail
+        recipeImg.append($("<figure>").attr("class", "image is-4by3")
+        .append($("<img>").attr("src", recipesResponse[i].recipe.images.REGULAR.url)));
+        console.log(recipesResponse[i].recipe.images.REGULAR.url);
 
-let map, infoWindow;
+        // CARD FOOTER
+        var recipeData = $("<div>").attr("class", "card-footer");
+        
+        // Cuisine Type
+        recipeData.append($("<div>").attr("class", "card-footer-item").text(recipesResponse[i].recipe.cuisineType[0]));
+        console.log(recipesResponse[i].recipe.cuisineType[0]);
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 6,
-  });
-  infoWindow = new google.maps.InfoWindow();
+        // Prep Time
+        recipeData.append($("<div>").attr("class", "card-footer-item").text(recipesResponse[i].recipe.totalTime));
+        console.log(recipesResponse[i].recipe.totalTime);
 
-    window.addEventListener("load", () => {
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            (position) => {
-            const pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude,
-            };
+        recipeCard.append(recipeName);
+        recipeCard.append(recipeImg);
+        recipeCard.append(recipeData);
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent("Location found.");
-            infoWindow.open(map);
-            map.setCenter(pos);
-            },
-            () => {
-            handleLocationError(true, infoWindow, map.getCenter());
-            }
-        );
-        } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
+        contentContainer.append(recipeCard);
+
+
+        // Link to Recipe (On Modal)
+        var recipeLink = recipesResponse[i].recipe.url;
+        console.log(recipeLink);
+
+        // Meal Type (Maybe used to filter?)
+        var recipeMeal = recipesResponse[i].recipe.mealType[0];
+        console.log(recipeMeal);
+
+        for (var j = 0; j < recipesResponse[i].recipe.ingredientLines.length; j++) {
+            // Ingredients
+            console.log(recipesResponse[i].recipe.ingredientLines[j]);
         }
-    });
+
+        // REMOVE THIS WHEN WE AREN'T LOGGING TO CONSOLE
+        console.log("-----");
+    }
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
-}
+// let map, infoWindow;
 
-window.initMap = initMap;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 6,
+//   });
+//   infoWindow = new google.maps.InfoWindow();
 
-console.log("A change was made!");
+//     window.addEventListener("load", () => {
+//         // Try HTML5 geolocation.
+//         if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(
+//             (position) => {
+//             const pos = {
+//                 lat: position.coords.latitude,
+//                 lng: position.coords.longitude,
+//             };
 
-searchBttn.on("click", getResponse);
+//             infoWindow.setPosition(pos);
+//             infoWindow.setContent("Location found.");
+//             infoWindow.open(map);
+//             map.setCenter(pos);
+//             },
+//             () => {
+//             handleLocationError(true, infoWindow, map.getCenter());
+//             }
+//         );
+//         } else {
+//         // Browser doesn't support Geolocation
+//         handleLocationError(false, infoWindow, map.getCenter());
+//         }
+//     });
+// }
+
+// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//   infoWindow.setPosition(pos);
+//   infoWindow.setContent(
+//     browserHasGeolocation
+//       ? "Error: The Geolocation service failed."
+//       : "Error: Your browser doesn't support geolocation."
+//   );
+//   infoWindow.open(map);
+// }
+
+// window.initMap = initMap;
+
+searchBttn.on("click", getRecipes);
 
